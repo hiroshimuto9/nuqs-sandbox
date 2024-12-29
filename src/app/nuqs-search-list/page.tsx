eecb9068557330256eaaf80b8e118f0e01924c75
Hiroshi Muto <mhi6o461o@gmail.com>
2024-12-29 17:02:05 +0900

    feat: supabeseに連携しUserテーブルからレコードを取得するまでの処理を実装

--- src/app/nuqs-search-list/page.tsx
+++ src/app/nuqs-search-list/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <div>Search list with nuqs</div>;
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+export default async function Page() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
+  const { data: users, error: usersError } = await supabase
+    .from("User")
+    .select("*");
+
+  return (
+    <>
+      <p>Hello {data.user.email}</p>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <pre>{JSON.stringify(usersError, null, 2)}</pre>
+    </>
+  );
 }
