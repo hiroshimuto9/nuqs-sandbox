274c540d1725a9ec2cd92ebaf7a57929f127b1ee
Hiroshi Muto <mhi6o461o@gmail.com>
2025-01-01 15:36:51 +0900

    feat: Tableコンポーネントの追加

--- src/app/nuqs-search-list/page.tsx
+++ src/app/nuqs-search-list/page.tsx
@@ -1,5 +1,14 @@
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default async function Page() {
   const supabase = await createClient();
 
@@ -14,9 +23,25 @@ export default async function Page() {
 
   return (
     <>
-      <p>Hello {data.user.email}</p>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-      <pre>{JSON.stringify(usersError, null, 2)}</pre>
+      <div className="mb-4">
+        <p>Hello {data.user.email}</p>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Name</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {users?.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
   );
 }
