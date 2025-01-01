981c68e4e0732a8b87cbb9ac0288448a30ac2c0b
Hiroshi Muto <mhi6o461o@gmail.com>
2025-01-01 19:16:47 +0900

    feat: クエリ更新処理を実装

--- src/app/nuqs-search-list/_components/SearchField.tsx
+++ src/app/nuqs-search-list/_components/SearchField.tsx
@@ -1,9 +1,26 @@
+"use client";
+import { useState, ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 
 export function SearchField() {
+  const [search, setSearch] = useState<string>("");
+  const router = useRouter();
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearch(value);
+    router.push(`?search=${value}`);
+  };
+
   return (
     <div className="mb-4">
-      <Input type="text" placeholder="name" />
+      <Input
+        type="text"
+        placeholder="name"
+        value={search}
+        onChange={handleSearchChange}
+      />
     </div>
   );
 }
