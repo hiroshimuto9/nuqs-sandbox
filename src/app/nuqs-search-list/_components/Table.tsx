65e80f912033d226a262207f3a4bdbf5eff61e88
Hiroshi Muto <mhi6o461o@gmail.com>
2025-01-02 14:59:32 +0900

    feat: 検索機能の実装

--- src/app/nuqs-search-list/_components/Table.tsx
+++ src/app/nuqs-search-list/_components/Table.tsx
@@ -8,29 +8,35 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-export async function SearchTable() {
-  const supabase = await createClient();
 
+export async function SearchTable({ search }: { search: string }) {
+  const supabase = await createClient();
   const { data: users, error: usersError } = await supabase
     .from("User")
-    .select("*");
+    .select()
+    .ilike("name", `%${search}%`);
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">ID</TableHead>
-          <TableHead>Name</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {users?.map((user) => (
-          <TableRow key={user.id}>
-            <TableCell>{user.id}</TableCell>
-            <TableCell>{user.name}</TableCell>
+    <>
+      <div className="bg-blue-300 my-4 p-2 rounded">
+        searchParams : {search}
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Name</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {users?.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
--- src/app/nuqs-search-list/page.tsx
+++ src/app/nuqs-search-list/page.tsx
@@ -2,15 +2,28 @@ import { Suspense } from "react";
 import { SearchField } from "./_components/SearchField";
 import { SearchTable } from "./_components/Table";
 
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string }>;
+}) {
   return (
     <>
       <div className="mb-4">
         <SearchField />
       </div>
       <Suspense fallback="Loading...">
-        <SearchTable />
+        <SearchTableWrapper searchParams={searchParams} />
       </Suspense>
     </>
   );
+}
+
+async function SearchTableWrapper({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string }>;
+}) {
+  const search = (await searchParams).search;
+  return <SearchTable search={search} />;
 }
